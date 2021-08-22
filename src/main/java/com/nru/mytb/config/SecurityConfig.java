12c51650b5f65365464e9ac0c192729a7335f2c8
hskoo <nrudev812@gmail.com>
2021-08-22 20:42:22 +0900

    refactor : 로그인시 비밀번호 해쉬화
    - 로그인 시에 작성하는 password를 같은 해쉬로 암호화하여 db의 해쉬된 비밀번호와 비교하기 위함.

--- src/main/java/com/nru/mytb/config/SecurityConfig.java
+++ src/main/java/com/nru/mytb/config/SecurityConfig.java
@@ -1,20 +1,31 @@
 package com.nru.mytb.config;
 
+import com.nru.mytb.config.auth.PrincipalDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PrincipalDetailsService principalDetailsService;
+
     @Bean
     public BCryptPasswordEncoder pwdEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(principalDetailsService).passwordEncoder(pwdEncoder());
     }
 
     @Override

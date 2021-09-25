914c47ee61e285697cf534eaa52fb30e7b6a1285
hskoo <nrudev812@gmail.com>
2021-09-26 00:24:03 +0900

    refactor : ServletInitializer 위치 변경

--- src/main/java/com/nru/mytb/MyTbApplication.java
+++ src/main/java/com/nru/mytb/MyTbApplication.java
@@ -1,15 +1,15 @@
-package com.nru.mytb;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@SpringBootApplication
-public class MyTbApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MyTbApplication.class, args);
-    }
-
-}
+//package com.nru.mytb;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//@EnableJpaAuditing
+//@SpringBootApplication
+//public class MyTbApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MyTbApplication.class, args);
+//    }
+//
+//}
